925c9f6a8b593734f5386f309388016063324de9
justin-vt <=>
2025-03-03 15:38:00 -0500

    renamed

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='brush-strokes-comfyui',
+    name='ComfyUI-brushstrokes',
     version='0.1.0',
     packages=find_packages(),
     install_requires=['Pillow', 'wand', 'gmic-py'],
     description='A ComfyUI node for painterly brush strokes using ImageMagick and G\'MIC',
-    author='Your Name',
-    url='https://github.com/YourUser/brush-strokes-comfyui',
+    author='Justin Pinder',
+    url='https://github.com/justin-vt/comfyui-brushstrokes',
 )
