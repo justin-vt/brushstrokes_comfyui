b87d073fcb901e4c1508e1aaed7a99827175c116
justin-vt <justin_vt@yahoo.com>
2025-03-03 16:59:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,19 @@ setup(
     name='ComfyUI-brushstrokes',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=['Pillow', 'wand', 'gmic-py'],
+    include_package_data=True,
+    install_requires=[
+        'Pillow>=9.0.0',
+        'wand>=0.6.10',
+        'gmic-py>=2.9.0',
+    ],
     description='A ComfyUI node for painterly brush strokes using ImageMagick and G\'MIC',
     author='Justin Pinder',
-    url='https://github.com/justin-vt/comfyui-brushstrokes',
+    url='https://github.com/justin-vt/ComfyUI-brushstrokes',
+    keywords=['ComfyUI', 'Image Processing', 'ImageMagick', 'Gmic'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.8'
 )
