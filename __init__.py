05dd144708b3ff1592cef0ee91320de6a31eccd9
justin-vt <=>
2025-03-05 12:58:42 -0500

    removed old nodes

--- __init__.py
+++ __init__.py
@@ -1,6 +1,4 @@
-from .nodes import PILBrushStrokesNode 
 from .nodes import OpenCVBrushStrokesNode 
-from .nodes import WandBrushStrokesNode 
 
 NODE_CLASS_MAPPINGS = {
     "OpenCVBrushStrokesNode": OpenCVBrushStrokesNode\
